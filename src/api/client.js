b12067a25dacbef7742a025d04abc5817bc2cd3b
tRoNgDuC159 <trongducnguyenhuynhf@gmail.com>
2025-12-27 17:27:08 +0700

    Code UI / UX mới nhất

--- src/api/client.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Thay đổi baseURL tùy theo server của bạn
-const client = axios.create({
-  baseURL: "http://localhost:8080", 
-  headers: { "Content-Type": "application/json" },
-});
-
-export default client;
