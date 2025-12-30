be030fa8a9809bd5eaa0241b8dc2b73af4af9a95
HnidChuan <hnidchuan@gmail.com>
2025-12-30 11:37:28 +0700

    Added to admin

--- src/Admin_Frontend/src/Candidate_Frontend/src/api/client.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:8080",
-  headers: { "Content-Type": "application/json" },
-  withCredentials: false,
-});
-
-export default client;
--- src/Admin_Frontend/src/Candidate_Frontend/src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Logo() {
-  return (
-    <div className="logo">
-      <h1>TopCV <span>hihi</span></h1>
-    </div>
-  );
-}
