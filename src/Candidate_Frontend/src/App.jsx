b382c8c4f45784ea989a61f7144d2340de4cdb9f
duyhao7777 <daoduyhao7777@gmail.com>
2026-01-09 23:59:58 +0700

    Add: page terms and condition

--- src/Candidate_Frontend/src/App.jsx
+++ src/Candidate_Frontend/src/App.jsx
@@ -22,6 +22,7 @@ import ResetPasswordPage from "./pages/ResetPasswordPage.jsx";
 import EmployerProfilePage from "./pages/EmployerProfile.jsx";
 import About from "./pages/About.jsx";
 import Policies from "./pages/Policies_page.jsx";
+import Terms from "./pages/TermsAndConditions.jsx"
 
 function App() {
   const location = useLocation();
@@ -39,6 +40,7 @@ function App() {
       <div className="flex-1 overflow-hidden relative">
         <div className="h-full w-full overflow-y-auto">
           <Routes>
+            <Route path="/terms" element={<Terms />} />
             <Route path="/policies" element={<Policies />} />
             <Route path="/about" element={<About />} />
             <Route path="/jobs" element={<JobSearchPage />} />
