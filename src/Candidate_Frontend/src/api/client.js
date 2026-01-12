9cb1995e59bf6e18b237a11921e0843407c41717
duyhao7777 <daoduyhao7777@gmail.com>
2026-01-12 23:55:05 +0700

    fix post api client

--- src/Candidate_Frontend/src/api/client.js
+++ src/Candidate_Frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8500" || "https://jobportal-server-6g2n.onrender.com",
+  baseURL: "http://localhost:8080" || "https://jobportal-server-6g2n.onrender.com",
   headers: { "Content-Type": "application/json" },
   withCredentials: false,
 });
