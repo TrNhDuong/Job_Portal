7ba28dcab4eabab663d02a71d1ccd31246f6b44a
TrNhDuong <nhatduong0411@gmail.com>
2026-01-13 11:00:43 +0700

    Update latest, serve for deploy

--- src/Candidate_Frontend/src/api/client.js
+++ src/Candidate_Frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8080" || "https://jobportal-server-6g2n.onrender.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://job-portal-la9s.onrender.com",
   headers: { "Content-Type": "application/json" },
   withCredentials: false,
 });
